6069ddc2a70302b959e59c99f8d64bf497ef52cd
Kriti Sharma <138896603+KritiSharma18@users.noreply.github.com>
2026-09-17 23:17:58 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+        int counts[26] = {0};
+        for (int i = 0; i < s.size(); i++) {
+            counts[s[i] - 'a']++;
+            counts[t[i] - 'a']--;
+        }
+        for (int c : counts) if (c != 0) return false;
+        return true;
+    }
+};
